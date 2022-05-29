525e4b5b4ffa8c893bfc9d99a04ec90c37a7fbb2
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2022-05-29 02:16:12 -0300

    User repository for RuimDatabase

---
+++ src/Adapters/RuimDatabase/UserRepository.js
@@ -0,0 +1,40 @@
+import { RuimDatabase } from '../../../lib/RuimDatabase/RuimDatabase.js';
+import { randomBytes } from 'crypto' // For generating random IDs
+
+export default class userRepository
+{
+  constructor() {
+    this.db = new RuimDatabase(process.env.STORAGE_PATH + '/users');
+  }
+  getUserById(id) {
+    return this.db.get(id);
+  }
+  async getUserByEmail(email) {
+    if (typeof email !== 'string') {
+      throw new Error('Email must be a string');
+    }
+
+    const users = await this.db.getAll('');
+    return users.find(user => user.email === email);
+  }
+  getUsers(filters) { // @todo use filters
+    return this.db.getAll('');
+  }
+  async createUser(user) {
+    let id = '';
+
+    do id = randomBytes(16).toString('hex');
+    while (this.db.exists(id));
+
+    await this.db.save(user, id);
+    user.id = id;
+    return user;
+  }
+  updateUser(id, user) {
+    user.id = id;
+    return this.db.save(user, id);
+  }
+  deleteUser(id) {
+    return this.db.delete(id);
+  }
+}
